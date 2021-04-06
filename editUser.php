9687ab29902741f6116d70a97a463cbbe855ed43
Sandra-beep <sandee.rivas@gmail.com>
2021-04-06 06:00:09 +0200

    testade lite mer på edituser

--- editUser.php
+++ editUser.php
@@ -3,10 +3,6 @@
 session_start();
 include('db.php');
 
-// Get-variabeln action från form
-$action = $_GET['action'];
-$id = $_GET['id'];
-
 if(isset($_GET['action'])){
  $action = $_GET['action'];
 }else {
@@ -15,12 +11,20 @@ if(isset($_GET['action'])){
 
 
 if( isset($action) && $action =="update" ){
+
+    $id = $_SESSION['id'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
     $sql = "UPDATE users SET Username=:username_IN, email_IN, password_IN WHERE ID=:id_IN";
     $stm = $pdo->prepare( $sql );
-    $stm-> bindParam('username_IN', $_POST['username']);
-    $stm-> bindParam('email_IN', $_POST['email']);
-    $stm-> bindParam('password_IN', $_POST['password']);
-    $stm-> bindParam('id_IN', $_POST['id']);
+
+    $stm-> bindParam('id_IN', $id);
+    $stm-> bindParam('username_IN', $username);
+    $stm-> bindParam('email_IN', $email);
+    $stm-> bindParam('password_IN', $password);
+
 
     if ( $stm->execute() ){
         header('location:editUser.php');
@@ -33,29 +37,38 @@ if( isset($action) && $action =="update" ){
 
 // Hämtar användare från databasen, tabellen users
 $stm = $pdo->prepare('SELECT ID, Username, Email, Password FROM users WHERE ID=:id_IN');
+
 $stm->bindParam(":id_IN", $id);
+$stm->execute();
 
 if(!$stm->execute()){
     echo "Ops! Kunde inte uppdatera info!";
    die();
 } 
 
-$userData = $stm->fetch();
+$stm->fetch();
+
 
 ?>
+
+
+
 
 <!-- Själva formen med möjlighet att ändra-->
 <form method="POST" action="editUser.php?action=update"> 
 <input type ="hidden" name ="id" value="<?=$_GET['id']?>">
-<h2>Uppdatera dina uppgifter här  <?php echo $_SESSION ['username']?>:</h2>
+<br>
 <button><a href="entries.php">Starsida</a></button>
-<button><a href="index.php">Logga ut</a></button> <br><br><br>
+<button><a href="index.php">Logga ut</a></button> <br>
+<h2>Uppdatera här  <?php echo $_SESSION ['username']?>:</h2>
+<br><br>
 
 Username:<br> <!-- ['username']? Från databas-tabellen eller form? -->
-<input type = "text" name = "username" value = "<?=$userdata['Username']?>" /> <br> 
+<input type = "text" name = "username" placeholder = "<?php echo $_SESSION ['username']?>" /> <br> 
 Email:<br>
-<input type = "text" name = "email" value = "<?=$userdata['Email']?>"  /> <br>
-Password:<br>
-<input type = "text" name = "password" value = "<?=$userdata['Password']?>"/> <br><br>
+<input type = "text" name = "email" value = "<?php echo $email;?>"  /> <br>
 
-<input type = "submit" name = "submit" value = "Uppdatera!">
+Password:<br>
+<input type = "text" name = "password" value = "<?php echo $password;?>"/> <br><br>
+
+<input type = "submit" name = "update" value = "Uppdatera!">
